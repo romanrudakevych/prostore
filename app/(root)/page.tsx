d27e39c85a0b3be69771568b4512b460e2648bc7
RomanRDQ <roood90@gmail.com>
2025-04-10 14:39:18 +0200

    add getLatestProducts functionality through Prisma

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,13 @@
-import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
-  return <div>
-    <ProductList data={sampleData.products}  title="Newest arrivals" limit={4}/>
-  </div>;
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <div>
+      <ProductList data={latestProducts} title="Newest arrivals" limit={4} />
+    </div>
+  );
 };
 
 export default Homepage;
