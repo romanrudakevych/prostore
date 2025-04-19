f55ffc2f622d9e5dfe5981a5a717d4c5517d1908
RomanRDQ <roood90@gmail.com>
2025-04-19 19:15:47 +0200

    Prisma user-based models

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,32 +1,44 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
+// import { Pool, neonConfig } from '@neondatabase/serverless';
+// import { PrismaNeon } from '@prisma/adapter-neon';
+// import { PrismaClient } from '@prisma/client';
+// import ws from 'ws';
 
-// Sets up WebSocket connections, which enables Neon to use WebSocket communication.
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${process.env.DATABASE_URL}`;
+// // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
+// neonConfig.webSocketConstructor = ws;
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-// Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
-const pool = new Pool();
+// // Creates a new connection pool using the provided connection string, allowing multiple concurrent connections.
+// const pool = new Pool();
 
-// Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
-const adapter = new PrismaNeon(pool);
+// // Instantiates the Prisma adapter using the Neon connection pool to handle the connection between Prisma and Neon.
+// const adapter = new PrismaNeon(pool);
 
-// Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
-export const prisma = new PrismaClient().$extends({
-  result: {
-    product: {
-      price: {
-        compute(product) {
-          return product.price.toString();
-        },
-      },
-      rating: {
-        compute(product) {
-          return product.rating.toString();
-        },
-      },
-    },
-  },
-});
+// // Extends the PrismaClient with a custom result transformer to convert the price and rating fields to strings.
+// export const prisma = new PrismaClient().$extends({
+//   result: {
+//     product: {
+//       price: {
+//         compute(product) {
+//           return product.price.toString();
+//         },
+//       },
+//       rating: {
+//         compute(product) {
+//           return product.rating.toString();
+//         },
+//       },
+//     },
+//   },
+// });
+
+
+
+// New version of usage of Prisma adapter
+
+import { PrismaClient } from "@prisma/client"
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
