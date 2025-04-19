cbc89e658a1e7d782c56a4eb5b0cfc9fa5070cf9
RomanRDQ <roood90@gmail.com>
2025-04-19 19:14:35 +0200

    ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // Will still allow production build with type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
