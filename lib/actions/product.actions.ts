76541f1a3cbf18ade80b0fc5bb1508c028b9a913
RomanRDQ <roood90@gmail.com>
2025-06-14 17:59:18 +0200

    get products for admin page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,10 @@
 "use server";
 import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
-import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 
 // Get latest products
 export async function getLatestProducts() {
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: "desc" },
@@ -14,10 +13,31 @@ export async function getLatestProducts() {
   return convertToPlainObject(data);
 }
 
-
 //Get single product by it's slug
 export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
-    where: { slug: slug }
-  })
+    where: { slug: slug },
+  });
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return { data, totalPages: Math.ceil(dataCount / limit) };
 }
