7748fe70215ff038ed5ba57d90cfa0753c1ec32c
Roman Rudakevych <rudakevycroman.andrijovycrudakevyc@publicisgroupe.cz>
2025-07-13 18:41:24 +0200

    get filter URL function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -21,6 +21,31 @@ const SearchPage = async (props: {
     page = "1",
   } = await props.searchParams;
 
+  // Construct filter url
+  const getFilterUrl = ({
+    c,
+    p,
+    s,
+    r,
+    pg,
+  }: {
+    c?: string;
+    p?: string;
+    s?: string;
+    r?: string;
+    pg?: string;
+  }) => {
+    const params = { q, category, price, rating, sort, page };
+
+    if (c) params.category = c;
+    if (p) params.price = p;
+    if (s) params.sort = s;
+    if (r) params.rating = r;
+    if (pg) params.page = pg;
+
+    return `/search?${new URLSearchParams(params).toString()}`;
+  };
+
   const products = await getAllProducts({
     query: q,
     category,
@@ -33,7 +58,6 @@ const SearchPage = async (props: {
   return (
     <div className="grid md:grid-cols-5 md:gap-5">
       <div className="filter-links">{/* FILTER LINKS */}</div>
-
       <div className="md:col-span-4 space-y-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && <div>No products found</div>}
