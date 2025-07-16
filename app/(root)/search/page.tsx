e0bb38d10893153135bbb2b2e11243e5ce293a6b
Roman Rudakevych <rudakevycroman.andrijovycrudakevyc@publicisgroupe.cz>
2025-07-16 13:47:12 +0200

    sorting products fixed

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -31,6 +31,8 @@ const prices = [
 
 const ratings = [4, 3, 2, 1];
 
+const sortOrders = ["newest", "lowest", "highest", "rating"];
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string;
@@ -199,7 +201,18 @@ const SearchPage = async (props: {
               </Button>
             ) : null}
           </div>
-          <div>{/* SORT */}</div>
+          <div>
+            Sort by{" "}
+            {sortOrders.map((s) => (
+              <Link
+                key={s}
+                className={`mx-2 ${sort == s && "font-bold"}`}
+                href={getFilterUrl({ s })}
+              >
+                {s}
+              </Link>
+            ))}
+          </div>
         </div>
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
           {products.data.length === 0 && <div>No products found</div>}
